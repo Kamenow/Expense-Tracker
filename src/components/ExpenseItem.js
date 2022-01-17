fd1e9798d532acda727bd0a2969eb0ea53cce667
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-01-17 17:56:57 +0200

    changed date styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,23 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ props: { title, date, amount } }) => {
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+
   return (
     <div className='expense-container'>
       <div className='expense-item-description'>
+        <div className='expense-item-date'>
+          <div className='date-wrapper'>
+            <div className='expense-month'>{month}</div>
+            <div className='expense-year'>{year}</div>
+          </div>
+          <div>
+            <div className='expense-date'>{day}</div>
+          </div>
+        </div>
         <h2 className='expense-item-title'>{title}</h2>
-        <div className='expense-item-date'>{`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`}</div>
       </div>
       <div className='expense-item-price'>{amount}</div>
     </div>
