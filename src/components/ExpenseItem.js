589feb716d4e70a932898754e1b28c0e42bdbd6d
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-01-17 21:43:49 +0200

    expense item update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,14 @@
+import ExpenseDate from './ExpenseDate.js';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ props: { title, date, amount } }) => {
-  const month = date.toLocaleString('en-US', { month: 'long' });
-  const day = date.toLocaleString('en-US', { day: '2-digit' });
-  const year = date.getFullYear();
-
   return (
     <div className='expense-container'>
       <div className='expense-item-description'>
-        <div className='expense-item-date'>
-          <div className='date-wrapper'>
-            <div className='expense-month'>{month}</div>
-            <div className='expense-year'>{year}</div>
-          </div>
-          <div>
-            <div className='expense-date'>{day}</div>
-          </div>
-        </div>
+        <ExpenseDate date={date} />
         <h2 className='expense-item-title'>{title}</h2>
       </div>
-      <div className='expense-item-price'>{amount}</div>
+      <div className='expense-item-price'>{amount}$</div>
     </div>
   );
 };
