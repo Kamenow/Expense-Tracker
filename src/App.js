8b4a096025bfe55f3c48ca14a211020d5d6da0a4
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-01-17 16:49:06 +0200

    dynamic component rendering

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import ExpenseItem from './components/ExpenseItem.js';
 import './App.css';
 
 function App() {
+  const expenses = [
+    {
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      title: 'Gosho',
+      amount: 523.23,
+      date: new Date(2020, 5, 14),
+    },
+    {
+      title: 'Kiro',
+      amount: 728.54,
+      date: new Date(2021, 8, 17),
+    },
+    {
+      title: 'Tosho',
+      amount: 348.74,
+      date: new Date(2020, 9, 29),
+    },
+  ];
+
   return (
     <div className='container'>
-      <ExpenseItem />
-      <ExpenseItem />
-      <ExpenseItem />
-      <ExpenseItem />
-      <ExpenseItem />
-      <ExpenseItem />
+      {expenses.map((expense, index) => {
+        return <ExpenseItem props={expense} key={index} />;
+      })}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
-  console.log(expenseDate);
-
+const ExpenseItem = ({ props: { title, date, amount } }) => {
   return (
     <div className='expense-container'>
       <div className='expense-item-description'>
-        <h2 className='expense-item-title'>{expenseTitle}</h2>
-        <div className='expense-item-date'>{`${expenseDate.getFullYear()}-${expenseDate.getMonth()}-${expenseDate.getDate()}`}</div>
+        <h2 className='expense-item-title'>{title}</h2>
+        <div className='expense-item-date'>{`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`}</div>
       </div>
-      <div className='expense-item-price'>{expenseAmount}</div>
+      <div className='expense-item-price'>{amount}</div>
     </div>
   );
 };
