67b64310a86c9cc54067a27728239f97d91554f9
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-01-18 18:53:47 +0200

    added dynamic stateful list

--- src/App.js
+++ src/App.js
@@ -3,35 +3,36 @@ import './App.css';
 import NewExpense from './components/NewExpense.js/NewExpense.js';
 import { useState } from 'react';
 
-function App() {
-  const [expenses, setExpenses] = useState([
-    {
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      title: 'Gosho',
-      amount: 523.23,
-      date: new Date(2020, 5, 14),
-    },
-    {
-      title: 'Kiro',
-      amount: 728.54,
-      date: new Date(2021, 8, 17),
-    },
-    {
-      title: 'Tosho',
-      amount: 348.74,
-      date: new Date(2020, 9, 29),
-    },
-  ]);
+const DUMMY_EXPENSES = [
+  {
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    title: 'Gosho',
+    amount: 523.23,
+    date: new Date(2020, 5, 14),
+  },
+  {
+    title: 'Kiro',
+    amount: 728.54,
+    date: new Date(2021, 8, 17),
+  },
+  {
+    title: 'Tosho',
+    amount: 348.74,
+    date: new Date(2020, 9, 29),
+  },
+];
 
-  // const expenses = ;
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
